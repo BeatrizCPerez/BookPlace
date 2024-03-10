ea9ffeaa29d2131fd8fb81f6b83f07489ee6bf17
BeaCp123 <beatrizcustodio262@gmail.com>
2024-03-11 00:45:45 +0100

    responsive better

--- client/src/components/Nav.jsx
+++ client/src/components/Nav.jsx
@@ -1,81 +1,75 @@
-import React, { useState, useEffect } from 'react';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faBars, faTimes } from '@fortawesome/free-solid-svg-icons';
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 
 const Nav = () => {
-  const [showMobileNav, setShowMobileNav] = useState(false);
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
 
-  const toggleMobileNav = () => {
-    setShowMobileNav(!showMobileNav);
+  const toggleMenu = () => {
+    setIsMenuOpen(!isMenuOpen);
   };
 
-  useEffect(() => {
-    const handleResize = () => {
-      const screenWidth = window.innerWidth;
-
-      setShowMobileNav(screenWidth < 768);
-    };
-
-    handleResize();
-
-    window.addEventListener('resize', handleResize);
-
-    return () => {
-      window.removeEventListener('resize', handleResize);
-    };
-  }, []);
-
   return (
-    <nav className={`w-screen h-fit overflow-hidden fixed z-50 bg-transparent border-b-2 border-white`}>
-      <div className="py-4 lg:px-8 px-4 max-w-[1280px] h-16 m-auto text-white flex items-center justify-between">
-        <Link to="/">
-          <button className="bg-blueGray-800 text-white active:bg-blueGray-600 text-sm font-bold uppercase px-6 py-3 rounded shadow hover:shadow-lg outline-none focus:outline-none mr-1 mb-1 w-full ease-linear transition-all duration-150" type="button">
-            VideoStore
-          </button>
+    <nav className="bg-black bg-opacity-30 p-4  fixed w-full z-20">
+      <div className="container mx-auto flex items-center justify-between">
+        <Link to="/" className="text-white font-bold text-xl">
+          VideoStore
         </Link>
-        <div className="hidden lg:flex gap-8 uppercase tracking-wider cursor-pointer text-lg items-center">
-        <span className="group">
-            Services
-            <div className="w-0 group-hover:w-full h-0.5 bg-white ease-in-out duration-500"></div>
-          </span>
-          <span className="group">
-            About
-            <div className="w-0 group-hover:w-full h-0.5 bg-white ease-in-out duration-500"></div>
-          </span>
-          <Link to="/LoginPage">
-            <span className="group bg-green-500 inline-block p-2 rounded-full hover:bg-blue-300">
-              LogIn
-              <div className="w-0 group-hover:w-full h-0.5 bg-white ease-in-out duration-500"></div>
-            </span>
-          </Link>
-          <Link to="/Registerpage">
-            <span className="group inline-block p-2 rounded-full border border-white hover:border-green-500 hover:text-green-500">
-              Register
-            </span>
-          </Link>
-        </div>
-        <div
-          id="hamburger"
-          className="lg:hidden flex items-center text-xl z-50"
-          onClick={toggleMobileNav}
+
+        <button
+          className="lg:hidden text-white focus:outline-none"
+          onClick={toggleMenu}
         >
-          {showMobileNav ? (
-            <FontAwesomeIcon icon={faTimes} />
+          {isMenuOpen ? (
+            <svg
+              className="h-6 w-6 transform rotate-180"
+              fill="none"
+              stroke="currentColor"
+              viewBox="0 0 24 24"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth="2"
+                d="M6 18L18 6M6 6l12 12"
+              ></path>
+            </svg>
           ) : (
-            <FontAwesomeIcon icon={faBars} />
+            <svg
+              className="h-6 w-6"
+              fill="none"
+              stroke="currentColor"
+              viewBox="0 0 24 24"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth="2"
+                d="M4 6h16M4 12h16m-7 6h7"
+              ></path>
+            </svg>
           )}
+        </button>
+
+        <div className="hidden md:flex space-x-4">
+          <Link to="/Loginpage" className="text-white text-xl hover:text-green-500">
+            Iniciar sesión
+          </Link>
+          <Link to="/Registerpage" className="text-white text-xl hover:text-blue-500">
+            Registrarse
+          </Link>
         </div>
-        {showMobileNav && (
-          <div
-            id="mobileNav"
-            className="lg:hidden fixed flex flex-col gap-8 pt-16 px-4 text-xl uppercase bg-teal-500 h-full inset-0 top-16 w-[70%] ease-in-out duration-500 cursor-pointer z-50"
-            style={{ backdropFilter: 'blur(10px)', backgroundColor: 'rgba(0, 0, 0, 0.5)' }}
-          >
-            <span>Servicios</span>
-            <span>Peliculas</span>
-            <Link to='/LoginPage'><span className='text-green-500 text-2xl'>LogIn</span></Link>
-            <Link to='/RegisterPage'><span className='text-white text-2xl'>Register</span></Link>
+
+        {isMenuOpen && (
+          <div className="lg:hidden absolute top-full left-0 right-0 bg-gray-800 bg-opacity-80 backdrop-filter backdrop-blur-lg p-4 rounded-md shadow-md">
+            {/* Contenido del menú desplegable */}
+            <Link to="/Loginpage" className="text-green-500 text-xl w-25 block py-2 hover:text-gray-500">
+              Iniciar sesión
+            </Link>
+            <Link to="/Registerpage" className="text-blue-600 text-xl block py-2  w-20 hover:text-green-500">
+              Registrarse
+            </Link>
           </div>
         )}
       </div>
