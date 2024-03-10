2057c3f76c129b0421f128d1a084e4948fc8db61
BeaCp123 <beatrizcustodio262@gmail.com>
2024-03-10 22:23:52 +0100

    Resposive contact ok

--- client/src/components/Contact.jsx
+++ client/src/components/Contact.jsx
@@ -7,6 +7,7 @@ const Contact = () => {
 
   const handleSocioChange = (e) => {
     setIsSocio(e.target.value === 'si');
+    setIsHacerseSocio(false);
   };
 
   const handleHacerseSocioChange = (e) => {
@@ -15,41 +16,40 @@ const Contact = () => {
 
   const handleSubmit = () => {
     Swal.fire('¡Solicitud enviada!', 'Su solicitud ha sido enviada correctamente.', 'success');
-    // Agrega aquí la lógica para enviar el formulario si es necesario
   };
 
   return (
-    <section className="text-gray-600 body-font bg-black relative flex flex-col lg:flex-row">
-      <div className="w-1/2  lg:w-1/2 relative h-64 lg:h-auto ml-20  bg-black flex justify-center">
-        <div className="bg-black p-2 text-center mt-11 text-gray-300">
-          <p className='hidden lg:block'>
-            <ol className='text-xl text-left ml-14 '>
-              <li className='p-2'>1. Rellena el formulario.</li>
-              <li className='p-2'>  Si quieres <span className='text-green-500'>información</span> solo rellena los datos de contacto, te contactaremos a la mayor brevedad posible.</li>
-              <li className='p-2'>2. Si deseas alquilar, completa el formulario.</li>
-              <li className='p-2'>3. Una vez envíes el formulario te informaremos si esta disponible tu película.</li>
-              <li className='p-2'>4. Si tu película está disponible, hacemos la reserva y abonarás el precio al recogerla.</li>
-              <li className='p-2'>5. Recuerda, tienes mínimo 48h para disfrutarla.</li>
-            </ol>
-          </p>
-          <div className='w-full lg:w-2/4 bg-black'>
-          <div className='hidden lg-block mt-10 ml-3'>
-            <iframe
-              title="map"
-              width="70%"
-              height="70%"
-              frameBorder="0"
-              marginHeight="0"
-              marginWidth="0"
-              scrolling="no"
-              src="https://maps.google.com/maps?width=100%&height=100%&hl=en&q=Puerta%20de%20Alcalá,%20Madrid&ie=UTF8&t=&z=16&iwloc=B&output=embed"
-            ></iframe>
+    <section className="text-gray-600 body-font bg-black relative flex flex-col lg:flex-row md:flex-row">
+      <div className=" md:w-1/2 lg:w-1/2   bg-black flex justify-center">
+        <div className="bg-black p-4 text-center mt-11 text-gray-300">
+          
+        <h2 className=' xl:text-xl xl:ml-14 sm:text-sm  text-left'>
+              <p className='p-2'>1. Rellena el formulario.</p>
+              <p className='p-2'> Si quieres <span className='text-green-500'>información</span> solo rellena los datos de contacto, te contactaremos a la mayor brevedad posible.</p>
+              <p className='p-2'>2. Si deseas alquilar, completa el formulario.</p>
+              <p className='p-2'>3. Una vez envíes el formulario te informaremos si está disponible tu película.</p>
+              <p className='p-2'>4. Si tu película está disponible, hacemos la reserva y abonarás el precio al recogerla.</p>
+              <p className='p-2'>5. Recuerda, tienes mínimo 48h para disfrutarla.</p>
+            </h2>
+         
+          <div className="w-full lg:w-2/4 bg-black">
+            <div className='hidden lg:block mt-10 ml-16'>
+              <iframe
+                title="map"
+                width="100%"
+                height="80%"
+                frameBorder="0"
+                marginHeight="0"
+                marginWidth="0"
+                scrolling="no"
+                src="https://maps.google.com/maps?width=100%&height=100%&hl=en&q=Puerta%20de%20Alcalá,%20Madrid&ie=UTF8&t=&z=16&iwloc=B&output=embed"
+              ></iframe>
+            </div>
           </div>
         </div>
       </div>
-    </div>
-
-      <div className="w-full lg:w-1/3 bg-gray-800 p-4 ml-auto mr-auto  mt-flex flex flex-col shadow-md">
+      
+      <div className="w-auto h-auto xl:h-2/6 xl:w-2/6 bg-gray-800 p-4 ml-auto mr-auto mt-20 flex flex-col shadow-md">
         <p className="block bg-green-500 text-white text-sm p-2 rounded-lg font-bold mb-4">
           ¿Alguna duda? Rellena el formulario y te contactaremos.
         </p>
@@ -63,37 +63,14 @@ const Contact = () => {
             Nombre
           </label>
           <input
-            type="Nombre"
+            type="text"
             id="name"
             name="name"
             autoComplete="name"
             className="w-full bg-gray-700 rounded border border-gray-600 focus:border-green-500 focus:ring-2 focus:ring-green-200 text-xs outline-none text-gray-100 py-1 px-2 leading-6 transition-colors duration-200 ease-in-out"
           />
         </div>
-        <div className="relative mb-2">
-          <label htmlFor="telefono" className="leading-7 text-xs text-gray-300">
-            Número de teléfono
-          </label>
-          <input
-            type="number"
-            id="telefono"
-            name="telefono"
-            autoComplete="tel"
-            className="w-full bg-gray-700 rounded border border-gray-600 focus:border-green-500 focus:ring-2 focus:ring-green-200 text-xs outline-none text-gray-100 py-1 px-2 leading-6 transition-colors duration-200 ease-in-out"
-          />
-        </div>
-        <div className="relative mb-2">
-          <label htmlFor="email" className="leading-7 text-xs text-gray-300">
-            Email
-          </label>
-          <input
-            type="email"
-            id="email"
-            name="email"
-            autoComplete="email"
-            className="w-full bg-gray-700 rounded border border-gray-600 focus:border-green-500 focus:ring-2 focus:ring-green-200 text-xs outline-none text-gray-100 py-1 px-2 leading-6 transition-colors duration-200 ease-in-out"
-          />
-        </div>
+
         <div className="relative mb-2">
           <label htmlFor="eresSocio" className="leading-7 text-xs text-gray-300">
             ¿Eres socio?
@@ -110,43 +87,87 @@ const Contact = () => {
         </div>
 
         {isSocio && (
+          <>
+            {/* Parte del formulario específica para socios */}
+            <div className="relative mb-2">
+              <label htmlFor="numeroSocio" className="leading-7 text-xs text-gray-300">
+                Número de socio
+              </label>
+              <input
+                type="text"
+                id="numeroSocio"
+                name="numeroSocio"
+                className="w-full bg-gray-700 rounded border border-gray-600 focus:border-green-500 focus:ring-2 focus:ring-green-200 text-xs outline-none text-gray-100 py-1 px-2 leading-6 transition-colors duration-200 ease-in-out"
+              />
+            </div>
+            <div className="relative mb-2">
+              <label htmlFor="peliculaDeseada" className="leading-7 text-xs text-gray-300">
+                ¿Qué película desea alquilar?
+                <span className="block text-xs text-gray-400 mt-1">En caso de querer información, dejar en blanco.</span>
+              </label>
+              <input
+                type="text"
+                id="peliculaDeseada"
+                name="peliculaDeseada"
+                className="w-full bg-gray-700 rounded border border-gray-600 focus:border-green-500 focus:ring-2 focus:ring-green-200 text-xs outline-none text-gray-100 py-1 px-2 leading-6 transition-colors duration-200 ease-in-out"
+              />
+            </div>
+          </>
+        )}
+
+        {isSocio ? (
           <div className="relative mb-2">
-            <label htmlFor="numeroSocio" className="leading-7 text-xs text-gray-300">
-              Número de socio
+            <label htmlFor="fechaAlquiler" className="leading-7 text-xs text-gray-300">
+              Fecha de alquiler
             </label>
             <input
-              type="text"
-              id="numeroSocio"
-              name="numeroSocio"
+              type="date"
+              id="fechaAlquiler"
+              name="fechaAlquiler"
               className="w-full bg-gray-700 rounded border border-gray-600 focus:border-green-500 focus:ring-2 focus:ring-green-200 text-xs outline-none text-gray-100 py-1 px-2 leading-6 transition-colors duration-200 ease-in-out"
             />
           </div>
+        ) : (
+          <>
+            {/* Resto del formulario para no socios */}
+            <div className="relative mb-2">
+              <label htmlFor="email" className="leading-7 text-xs text-gray-300">
+                Email
+              </label>
+              <input
+                type="text"
+                id="email"
+                name="email"
+                autoComplete="Email"
+                className="w-full bg-gray-700 rounded border border-gray-600 focus:border-green-500 focus:ring-2 focus:ring-green-200 text-xs outline-none text-gray-100 py-1 px-2 leading-6 transition-colors duration-200 ease-in-out"
+              />
+            </div>
+            <div className="relative mb-2">
+              <label htmlFor="quieresSerSocio" className="leading-7 text-xs text-gray-300">
+                ¿Quieres ser socio?
+              </label>
+              <select
+                id="quieresSerSocio"
+                name="quieresSerSocio"
+                className="w-full bg-gray-700 rounded border border-gray-600 focus:border-green-500 focus:ring-2 focus:ring-green-200 text-xs outline-none text-gray-100 py-1 px-2 leading-6 transition-colors duration-200 ease-in-out"
+                onChange={handleHacerseSocioChange}
+              >
+                <option value="si">Sí</option>
+                <option value="no">No</option>
+              </select>
+            </div>
+            <div className="relative mb-2">
+              <label htmlFor="comentario" className="leading-7 text-xs text-gray-300">
+                Comentario
+              </label>
+              <textarea
+                id="comentario"
+                name="comentario"
+                className="w-full bg-gray-700 rounded border border-gray-600 focus:border-green-500 focus:ring-2 focus:ring-green-200 text-xs outline-none text-gray-100 py-1 px-2 leading-6 transition-colors duration-200 ease-in-out"
+              ></textarea>
+            </div>
+          </>
         )}
-
-        <div className="relative mb-2">
-          <label htmlFor="fechaAlquiler" className="leading-7 text-xs text-gray-300">
-            Fecha de alquiler
-          </label>
-          <input
-            type="date"
-            id="fechaAlquiler"
-            name="fechaAlquiler"
-            className="w-full bg-gray-700 rounded border border-gray-600 focus:border-green-500 focus:ring-2 focus:ring-green-200 text-xs outline-none text-gray-100 py-1 px-2 leading-6 transition-colors duration-200 ease-in-out"
-          />
-        </div>
-
-        <div className="relative mb-2">
-          <label htmlFor="peliculaDeseada" className="leading-7 text-xs text-gray-300">
-            Película que quieres
-            <span className="block text-xs text-gray-400 mt-1">En caso de querer información, dejar en blanco.</span>
-          </label>
-          <input
-            type="text"
-            id="peliculaDeseada"
-            name="peliculaDeseada"
-            className="w-full bg-gray-700 rounded border border-gray-600 focus:border-green-500 focus:ring-2 focus:ring-green-200 text-xs outline-none text-gray-100 py-1 px-2 leading-6 transition-colors duration-200 ease-in-out"
-          />
-        </div>
 
         <button
           className="text-white bg-green-500 border-0 py-1 px-4 focus:outline-none hover:bg-green-600 rounded text-sm"
