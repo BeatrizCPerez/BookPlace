8c6fc58ecc03696a132fd57dfe558ffb91d413c6
BeaCp123 <beatrizcustodio262@gmail.com>
2024-03-16 16:48:14 +0100

    mm

--- client/src/components/Login.jsx
+++ client/src/components/Login.jsx
@@ -84,9 +84,9 @@ const Login = () => {
       <ScrollToTop />
       <section className="relative">
       <video autoPlay muted loop className="w-full h-full object-cover fixed inset-0 z-0">
-          <source src="https://res.cloudinary.com/djysp2khi/video/upload/v1710603298/yng09oaogupe1lysqugu.mp4" type="video/mp4" />
-          Tu navegador no admite la etiqueta de video.
-        </video>
+        <source src="https://res.cloudinary.com/djysp2khi/video/upload/v1710603298/yng09oaogupe1lysqugu.mp4" type="video/mp4" />
+        Tu navegador no admite la etiqueta de video.
+      </video>
         <Nav />
         <div className="w-full lg:w-4/12 px-4 mx-auto pt-16">
           <div className="relative flex mt-[20px] flex-col min-w-0 break-words w-full mb-2 shadow-lg bg-opacity-50 backdrop-filter backdrop-blur-lg rounded-lg bg-blueGray-200">
--- client/src/components/Register.jsx
+++ client/src/components/Register.jsx
@@ -63,9 +63,9 @@ const Register = () => {
       <section className="relative">
         <Nav />
         <video autoPlay muted loop className="w-full h-full object-cover fixed inset-0 z-0">
-          <source src="https://res.cloudinary.com/djysp2khi/video/upload/v1710603298/yng09oaogupe1lysqugu.mp4" type="video/mp4" />
-          Tu navegador no admite la etiqueta de video.
-        </video>
+        <source src="https://res.cloudinary.com/djysp2khi/video/upload/v1710603298/yng09oaogupe1lysqugu.mp4" type="video/mp4" />
+        Tu navegador no admite la etiqueta de video.
+      </video>
         <div className="w-full lg:w-4/12 px-4 mx-auto pt-6">
           <div className="relative flex mt-[40px] flex-col min-w-0 break-words w-full mb-6 shadow-lg bg-opacity-50 backdrop-filter backdrop-blur-lg rounded-lg bg-blueGray-200">
             <div className="rounded-t mb-0 px-6 py-6">
