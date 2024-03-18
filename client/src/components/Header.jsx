578c49415bcc6528bb70e5200c1e05bfc682c9c2
BeaCp123 <beatrizcustodio262@gmail.com>
2024-03-18 14:32:22 +0100

    pictures

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -48,26 +48,26 @@ const Header = () => {
     <div id='header' className="relative" style={{ height: '95vh', overflow: 'hidden' }}>
       <ScrollToTop />
       <img
-        src="https://okdiario.com/img/2022/11/22/libros-4-635x358.jpg"
+        src="https://img.freepik.com/foto-gratis/gran-coleccion-libros-antiguos-estantes-madera-generados-ia_188544-29739.jpg?w=826&t=st=1710610681~exp=1710611281~hmac=a9182aaade0001eab120d67dc952bb76d5046bd6df5cd01d3a247826426ada6d"
         alt="Fondo"
         className="absolute w-full h-full object-cover"
         style={{ zIndex: -1 }}
       />
-      <div className="py-12 md:py-16 lg:py-20 relative" style={{ zIndex: 1 }}>
-        <div className="absolute top-0 left-0 w-full h-full bg-black opacity-50"></div>
-        <div className="container m-auto px-6 space-y-8 text-white md:px-12 lg:px-20 relative z-10">
+      <div className="absolute top-0 left-0 w-full h-full" style={{ backgroundColor: 'rgba(0, 0, 0, 0.5)', zIndex: -1 }}></div>
+      <div className="py-12 md:py-16 lg:py-20 relative z-10">
+        <div className="container m-auto px-6 space-y-8 text-white md:px-12 lg:px-20">
           <Fade bottom cascade>
             <div className="justify-center text-center gap-6 md:text-left md:flex lg:items-center lg:gap-16">
-              <div className="order-last mb-6 space-y-6 md:mb-0 md:w-6/12 lg:w-6/12">
-                <h1 className="text-4xl text-gray-300 font-bold md:text-5xl relative z-20">
+              <div className="mb-6 space-y-6 md:mb-0 md:w-6/12 lg:w-6/12">
+                <h1 className="text-4xl text-gray-300 font-bold md:text-5xl">
                   <span className="relative mt-5 inline-block">
                     Reserva ya tu libro favorito!!
                   </span>
                 </h1>
-                <p className='text-xl relative z-20'>
+                <p className='text-xl'>
                   BookPlace el rincon donde podrás disfrutar de ediciones exclusivas. Suscribete!!
                 </p>
-                <p className="text-lg relative z-20">
+                <p className="text-lg">
                   Únete a nuestra comunidad de lectores ubicados en Madrid.
                 </p>
                 <div className="flex flex-row-reverse flex-wrap justify-center gap-4 md:mb-5 md:gap-6 md:justify-end">
@@ -89,36 +89,35 @@ const Header = () => {
                   </button>
                 </div>
               </div>
-              <div className="grid grid-cols-5 grid-rows-4 gap-4 md:w-5/12 lg:w-6/12 relative z-20">
-                <div className="col-span-2 row-span-4">
-                  <img
-                    src="https://th.bing.com/th/id/OIP.yp1w7G2fEAaLbduCQOBXawHaE7?w=800&h=533&rs=1&pid=ImgDetMain"
-                    className="rounded-full"
-                    width="640"
-                    height="960"
-                    alt="Libro 1"
-                    loading="lazy"
-                  />
-                </div>
-                <div className={`col-span-2 row-span-2 ${isMobile ? 'block' : 'hidden'}`}>
+              <div className="grid grid-cols-2 gap-4 md:w-6/12 lg:w-6/12 justify-center">
+                <div className="mb-4">
                   <img
                     src="https://t3.ftcdn.net/jpg/03/15/87/14/360_F_315871469_H0SM5Js5QGOGPQp319qEiR5jM7mZhfqp.jpg"
-                    className="w-full h-full object-cover object-top rounded-xl md:hidden"
-                    width="640"
-                    height="640"
+                    className="w-full object-cover rounded-xl"
                     alt="Libro 2"
-                    loading="lazy"
                   />
                 </div>
-                {isDesktop && (
-                  <div className="col-span-3 row-span-3">
-                    <img
-                      src="https://okdiario.com/img/2022/11/22/libros-4-635x358.jpg"
-                      alt="Fondo"
-                      className="w-full h-full object-cover object-top rounded-xl"
-                    />
-                  </div>
-                )}
+                <div className="mb-4">
+                  <img
+                    src="https://estaticos-cdn.prensaiberica.es/clip/6fe3113d-af11-41bd-84ac-9340c4ee95f0_16-9-discover-aspect-ratio_default_0.webp"
+                    className="w-full object-cover rounded-xl"
+                    alt="Libro 1"
+                  />
+                </div>
+                <div className="mb-4">
+                  <img
+                    src="https://th.bing.com/th/id/OIP.yp1w7G2fEAaLbduCQOBXawHaE7?w=800&h=533&rs=1&pid=ImgDetMain"
+                    className="w-full object-cover rounded-xl"
+                    alt="Libro 1"
+                  />
+                </div>
+                <div className="mb-4">
+                  <img
+                    src="https://img.freepik.com/foto-gratis/cerca-adultos-jovenes-disfrutando-lectura_23-2149134387.jpg?w=740&t=st=1710767966~exp=1710768566~hmac=3715ddc5e78f3636c2f1efb86d1c1c4b1592cdeaaebfed6a80396cd8d07a907f"
+                    className="w-full object-cover rounded-xl"
+                    alt="Libro 1"
+                  />
+                </div>
               </div>
             </div>
           </Fade>
